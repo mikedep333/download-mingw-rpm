51fcf272920367cc4d7f937ca3e0571c7047303e
Mike DePaulo <mikedep333@gmail.com>
2015-04-25 15:03:30 -0400

    Add a copyright line for myself

--- download-mingw-rpm.py
+++ download-mingw-rpm.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 #
 # Copyright (C) Maarten Bosmans 2011-2012
+# Copyright (C) Michael DePaulo 2015
 #
 # This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 # If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
